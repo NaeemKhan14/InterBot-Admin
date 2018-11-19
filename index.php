45da1f73d14ba274b69491a2c13499d6debf1573
Naeem Khan <naeemukhan14@gmail.com>
2018-11-19 23:08:14 +0400

    Front page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>InterBot Admin Panel</title>
+  </head>
+  <body>
+    <form action="processInput.php" method="post">
+      <input type="text" name="input_field">
+      <input type="submit" value="submit">
+    </form>
+
+    <a href="showData.php">Show Data</a>
+    <a href="deleteData.php">Empty Data</a>
+  </body>
+</html>
